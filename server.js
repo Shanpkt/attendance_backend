a50d365af7dedf8b74e169ffd9a27d8dbefa99e1
Shantanu <shantanupathak01@gmail.com>
2026-09-04 15:23:52 +0530

    first commit

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 // MONGODB CONNECTION
 // ==================================================
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = "mongodb+srv://wings:wings@cluster0.epqncfr.mongodb.net/attendance";
 
 if (!MONGODB_URI) {
   console.error(
